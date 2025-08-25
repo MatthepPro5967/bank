8284bf2daf792217da7719e15ba773bd32df5f54
Matthew Sliman <matthewmsliman@icloud.com>
2025-08-25 14:23:00 -0400

    Implement auth, appwrite, and sentry

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,31 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import * as Sentry from '@sentry/nextjs';
+      import type { Metadata } from 'next';
 
-export default function RootLayout({
+      // Add or edit your "generateMetadata" to include the Sentry trace data:
+      export function generateMetadata(): Metadata {
+        return {
+          // ... your existing metadata
+          other: {
+            ...Sentry.getTraceData()
+          }
+        };
+      }
+
+      
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: 'Matt', lastName: 'Mei' }
+  
+  const loggedIn = await getLoggedInUser();
+
+  if(!loggedIn) redirect('/');
   
   return (
     <main className="flex h-screen w-full font-inter">
---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8ed6b8141ade3083e17e5b7790938d63@o4509905985077248.ingest.us.sentry.io/4509905986650113",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
